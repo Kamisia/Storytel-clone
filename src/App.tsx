4f7d4ae54cd086b6c3979b98df077e8234326d37
Kamisia <kamila.samczuk@gmail.com>
2024-07-11 11:20:36 +0200

    Organize directory structure according to Atomic Design principles. Add sample atom - Button.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Button from "./components/atoms/Button.tsx";
 
 const App: React.FC = () => {
+  const buttonClick = (text) => {
+    console.log(text);
+  };
   return (
     <div>
       <h1>Just start</h1>
+      <Button text="Click Me" onClick={() => buttonClick("clicked btn")} />
     </div>
   );
 };
